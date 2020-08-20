fcaec83924ea3a7aac638089714f5849980ac846
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2020-08-19 17:18:08 -0700

    trying assignments

--- JS/Basic_javascript_1.js
+++ JS/Basic_javascript_1.js
@@ -1,2 +1,13 @@
-alert("My first JS!");
-Window.alert("my first JS")
+
+alert("my first js");
+
+
+var A = "this is a string";
+
+document.write(A);
+
+document.write("Lisa told Bart, \"Knock it off,Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind\""
++ "-Dr. Seuss")
