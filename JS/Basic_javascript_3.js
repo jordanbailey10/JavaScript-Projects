fa090889e0ee9fb121060bb161c7d7e19c4dd8a7
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2020-08-20 20:23:49 -0700

    Basic HTML 3

---
+++ JS/Basic_javascript_3.js
@@ -0,0 +1,4 @@
+function addition_Function() { //creating addition function
+    var addition = 12 + 12;//creating variable;
+    document.getElementById("Math").innerHTML = "12 + 12 =" + addition;
+}
