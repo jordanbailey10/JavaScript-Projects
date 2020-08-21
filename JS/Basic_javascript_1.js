d1014b8a2621f15f3761405ba8b0ee2289c7d824
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2020-08-20 17:43:24 -0700

    update basic javascript

--- JS/Basic_javascript_1.js
+++ JS/Basic_javascript_1.js
@@ -1,13 +1,30 @@
 
-alert("my first js");
+alert("my first js");//creating alert
 
+window.alert("my first js.");//creating alert window
 
-var A = "this is a string";
+var A = "this is a string <br>";//assigning a string variable
 
-document.write(A);
+document.write(A);//showing variable string
 
-document.write("Lisa told Bart, \"Knock it off,Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+document.write("Lisa told Bart, \"Knock it off,Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");//creating string with quotes
 
 document.write("\"Be who you are and say what you feel,"
-+ " because those who mind don\'t matter and those who matter don\'t mind\""
-+ "-Dr. Seuss")
++ " because those who mind don\'t matter and those who matter don\'t mind\""//Concatenating string
++ "-Dr.Suess <br>");
+
+
+var pet1 = "Hops <br> ";//assigning variable
+var pet2 = "Nubs <br>";//assigning varriable
+var pet3 = "Cheddar <br>";
+var Mypets = pet1+pet2+pet3;//concatening variables
+document.write(Mypets);
+var pet1 = pet1.fontcolor("green");//making font color green
+document.write(pet1);
+var pet2 = pet2.fontcolor("red");//making font color red
+document.write(pet2);
+var pet3 = pet3.fontcolor("orange");//making font color orange
+document.write(pet3);
+
+
+document.write(12+4);//creating expression
