b2f8acd8fdbbabff4a375a3f9647946181dfc575
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2020-08-23 11:43:15 -0700

    update

--- JS/Basic_javascript_6.js
+++ JS/Basic_javascript_6.js
@@ -15,6 +15,8 @@ var Jack = new Vehicle("Doge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford","Pinto", 1971, "Mustard");
 function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erick.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
+
+
 
