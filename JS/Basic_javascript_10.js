85f0468a6c03f58b28136b88dde65795400ef4c2
Jordan <68933944+jordanbailey10@users.noreply.github.com>
2020-08-27 15:38:05 -0700

    update

--- JS/Basic_javascript_10.js
+++ JS/Basic_javascript_10.js
@@ -1,7 +1,10 @@
-
-var i = 0;
+function call_Loop(){
+var i = 1;
+var text = "";
 while (i <= 10) {
     text += "The number is " + i;
     i++;
 }
 document.getElementById("Loop").innerHTML = text
+
+var Instruments = ["Guitar", "Drums", "Piano",
